3a0c1d22b8f38b2aec276aa189f4cfead93f12fe
Cesc <thuocnv1802@gmail.com>
2022-08-04 10:58:42 +0700

    fix: add datatype to refresh_token_id

--- migrations/20220803214130-ddl_create_user_table.sql
+++ migrations/20220803214130-ddl_create_user_table.sql
@@ -1,18 +1,18 @@
 -- +migrate Up
 CREATE TABLE users
 (
-    id         int          not null auto_increment,
-    full_name  varchar(100),
-    last_name  varchar(50)  not null,
-    first_name varchar(50)  not null,
-    login_id   varchar(50)  not null,
-    password   varchar(100) not null,
-    salt       varchar(100) not null,
-    refresh_token_id        not null,
-    status     int          not null default 1,
-    created_at timestamp null default current_timestamp,
-    updated_at timestamp null default current_timestamp on update current_timestamp,
-    deleted_at timestamp null,
+    id               int          not null auto_increment,
+    full_name        varchar(100),
+    last_name        varchar(50)  not null,
+    first_name       varchar(50)  not null,
+    login_id         varchar(50)  not null,
+    password         varchar(100) not null,
+    salt             varchar(100) not null,
+    refresh_token_id varchar(100) not null,
+    status           int          not null default 1,
+    created_at       timestamp null default current_timestamp,
+    updated_at       timestamp null default current_timestamp on update current_timestamp,
+    deleted_at       timestamp null,
     primary key (id)
 
 ) ENGINE = InnoDB
